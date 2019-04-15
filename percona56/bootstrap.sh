6a15ac39003ecf451a95f8a017d5810300e53f98
vkudlak <v.kudlak@mangotele.com>
2019-04-15 19:34:31 +0300

    percona

---
+++ percona56/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dir=$(find /var/dumps/mango-office.ru  -maxdepth 1 -mindepth 1  -type d  | egrep '[0-9]{4}(-[0-9]{2}){2}' | sort -n | tail -n  1)
+
+test -d "${dir}" -a -n "${dir}" && {
+
+	systemctl stop mysql
+	test -d /var/lib/mysql && {
+		rm -rf /var/lib/mysql/
+	}
+	time innobackupex --copy-back ${dir}
+	chown mysql:mysql -R /var/lib/mysql
+	chmod 750 /var/lib/mysql/
+
+	systemctl start mysql
+
+	mysql_upgrade
+	mysql -e 'flush privileges;'
+}
