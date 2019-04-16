36217defbe147c286b96e20af1441e9f4aa0c4c9
vkudlak <v.kudlak@mangotele.com>
2019-04-16 20:34:20 +0300

    many-changes

--- percona/bootstrap.sh
+++ percona/bootstrap.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+id
+ps axuf
+find /
+ls -lah /var/backup
+find /var/backup
 dir=$(find /var/dumps/mango-office.ru  -maxdepth 1 -mindepth 1  -type d  | egrep '[0-9]{4}(-[0-9]{2}){2}' | sort -n | tail -n  1)
+#time innobackupex --copy-back ${dir}
+#chown mysql:mysql -R /var/lib/mysql
+#chmod 750 /var/lib/mysql/
+#mysql_upgrade
+#mysql -e 'flush privileges;'
+
+exit 0
 
 test -d "${dir}" -a -n "${dir}" && {
 
